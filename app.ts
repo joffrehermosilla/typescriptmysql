f62c4b24fc8a070b6a0970f74d9b8d74da39a508
joffre <joffre.hermosilla@gmail.com>
2022-03-16 15:10:18 -0500

    primer tutorial de typescript mysql y node

---
+++ app.ts
@@ -0,0 +1,4 @@
+export const nombre = 'Joffre';
+
+
+console.log(nombre);
---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.nombre = 'Joffre';
+console.log(exports.nombre);
+//# sourceMappingURL=app.js.map
